2399e79cb95912b979c082eb3878e950f6dd4b85
K9999 <yz82@nau.edu>
2016-05-27 04:09:42 +0800

    add some collision detection

--- game.js
+++ game.js
@@ -1,27 +1,28 @@
 var GAME_WIDTH = 600;
 var GAME_HEIGHT = 600;
 var GAME_SCALE = 1;
-// var HORIZON_Y = GAME_HEIGHT/GAME_SCALE/2;
+
 
 var gameport = document.getElementById("gameport");
 var renderer = new PIXI.autoDetectRenderer(GAME_WIDTH,
                                            GAME_HEIGHT,
                                            {backgroundColor: 0x99D5FF});
+                                           
+                
 gameport.appendChild(renderer.view);
-
 var stage = new PIXI.Container();
+//---------main screen-----------------------------------
+
+
+//--------game screen--------------------------------------
+
 stage.scale.x = GAME_SCALE;
 stage.scale.y = GAME_SCALE;
 
-// Scene objects get loaded in the ready function
 var player;
 var world;
 
-// Character movement constants:
-var MOVE_LEFT = 1;
-var MOVE_RIGHT = 2;
-var MOVE_UP = 3;
-var MOVE_DOWN = 4;
+
 var MOVE_NONE = 0;
 
 // The move function starts or continues movement
@@ -34,20 +35,20 @@ function move() {
   player.moving = true;
   console.log("move");
   
-  if (player.direction == MOVE_LEFT) 
+  if (player.direction == 1) //go left
     createjs.Tween.get(player).to({x: player.x - 32}, 500).call(move);
   
-  if (player.direction == MOVE_RIGHT)
+  if (player.direction == 2) //go right
     createjs.Tween.get(player).to({x: player.x + 32}, 500).call(move);
 
-  if (player.direction == MOVE_UP)
+  if (player.direction == 3) //go up
     createjs.Tween.get(player).to({y: player.y - 32}, 500).call(move);
   
-  if (player.direction == MOVE_DOWN)
+  if (player.direction == 4) // go down
     createjs.Tween.get(player).to({y: player.y + 32}, 500).call(move);
 }
 
-// Keydown events start movement
+
 window.addEventListener("keydown", function (e) {
   e.preventDefault();
   if (!player) return;
@@ -57,14 +58,25 @@ window.addEventListener("keydown", function (e) {
   player.direction = MOVE_NONE;
 
   if (e.keyCode == 87)
-    player.direction = MOVE_UP;
-  else if (e.keyCode == 83)
-    player.direction = MOVE_DOWN;
-  else if (e.keyCode == 65)
-    player.direction = MOVE_LEFT;
-  else if (e.keyCode == 68)
-    player.direction = MOVE_RIGHT;
-
+    player.direction = 3;
+  if (e.keyCode == 83)
+    player.direction = 4;
+  if (e.keyCode == 65)
+    player.direction = 1;
+  if (e.keyCode == 68)
+    player.direction = 2;
+  //add some naive collision detection
+  if (player.x >= 760) 
+     player.x = 760;
+  else if (player.x <= 9) 
+     player.x = 8;
+  if (player.y > 760) 
+     player.y = 760;
+   else if (player.y <= 9) 
+     player.y = 8;
+    
+  
+  
   console.log(e.keyCode);
   move();
 });
@@ -82,9 +94,8 @@ PIXI.loader
   .add('map_json', 'map.json')
   .add('map', 'map.png')
   .add('face', 'assets/sillyfaceyellow.png')
+  .add("spritesheet.json")
   .load(ready);
-
-
 
 
 
@@ -94,25 +105,36 @@ function ready() {
   world = tu.makeTiledWorld("map_json", "map.png");
   stage.addChild(world);
 
-  //var red = world.getObject("red");
+  var frame = [];
+  frame.push(PIXI.Texture.fromFrame("rolling1.png"));
+  frame.push(PIXI.Texture.fromFrame("rolling2.png"));
+  frame.push(PIXI.Texture.fromFrame("rolling3.png"));
+  frame.push(PIXI.Texture.fromFrame("rolling4.png"));
   
-  player = new PIXI.Sprite(PIXI.loader.resources.face.texture);
-  //player.x = red.x;
-  //player.y = red.y;
-  //player.anchor.x = 0.0;
-  //player.anchor.y = 1.0;
+  
+  
+  
+  player = new PIXI.extras.MovieClip(frame);
+  player.play();
+  player.animationSpeed = 0.1;
   player.position.x = 0;
   player.position.y = 144;
  
   world.addChild(player);
-  //Find the entity layer
-  //var entity_layer = world.getObject("Entities");
-  //entity_layer.addChild(player);
   player.direction = MOVE_NONE;
   player.moving = false;
   animate();
 }
 
+
+function changeView(view){
+    for(var i = 0; i<stage.children.length; i++){
+        stage.children[i].visible = false;
+        stage.children[i].interactive = false;
+    }
+    view.visible = true;
+    view.interative = true;
+}
 
 
 function animate() {
